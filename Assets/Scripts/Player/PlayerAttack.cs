0fe8e81dcd817bd98e379e097bc8ed2b430aca53
Leo Lunev <loganlev@bk.ru>
2023-09-23 12:10:40 +0300

    Feat: Добавить PlayerAttack.

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using TMPro;
+using System.Threading.Tasks;
+
+public class PlayerAttack : MonoBehaviour
+{
+	[SerializeField] private Transform shootingPoint;
+	[SerializeField] private TextMeshProUGUI AmmoText;
+	[SerializeField] private GunInfo currentGun;
+	private Vector3 direction;
+	private bool isWithinRange;
+
+	private void Start()
+	{
+		currentGun.CurrentAmmo = currentGun.MagazineSize;
+		UpdateAmmoText();
+	}
+
+	public void Attack()
+	{
+		if (isWithinRange)
+		{
+			if (currentGun.CurrentAmmo > 0)
+			{
+				currentGun.CurrentAmmo--;
+				float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+				var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+				var newBullet = Instantiate(currentGun.Bullet, shootingPoint.position, rotation);
+				UpdateAmmoText();
+			}
+			else
+			{
+				Reload();
+			}
+		}
+	}
+
+	public void ChooseGun(GunInfo gun)
+	{
+		currentGun = gun;
+	}
+
+	public GunInfo GetCurrentGun()
+	{
+		return currentGun;
+	}
+	private void UpdateAmmoText()
+	{
+		AmmoText.text = currentGun.CurrentAmmo + "/" + currentGun.MagazineSize;
+	}
+
+	private async void Reload()
+	{
+		await Task.Delay((int)currentGun.ReloadTime*1000); // Заглушка, имитация перезарядки
+		Debug.Log("Reload");
+		currentGun.CurrentAmmo = currentGun.MagazineSize;
+		UpdateAmmoText();
+	}
+
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.GetComponent<EnemyBase>() != null)
+		{
+			var enemyPosition = collision.gameObject.transform.position;
+			direction = enemyPosition - transform.position;
+			isWithinRange = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.GetComponent<EnemyBase>() != null)
+		{
+			isWithinRange = false;
+		}
+	}
+}
