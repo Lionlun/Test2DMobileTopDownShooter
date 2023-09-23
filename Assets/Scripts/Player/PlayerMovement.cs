bcb24b3612d1a92b89b8a0944630c719e133e0aa
Leo Lunev <loganlev@bk.ru>
2023-09-23 12:40:17 +0300

    Feat: Добавить игроку анимации Idle, Run.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerMovement : MonoBehaviour, IDataSave
 {
 	[SerializeField] private Joystick joystick;
+	private Animator animator;
 	private Rigidbody2D rb;
 	private int speed = 3;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 
 	private void FixedUpdate()
@@ -19,6 +21,8 @@ public class PlayerMovement : MonoBehaviour, IDataSave
 	private void Move()
 	{
 		rb.velocity = new Vector3(joystick.Horizontal*speed, joystick.Vertical*speed, 0);
+		Debug.Log(rb.velocity.magnitude);
+		animator.SetFloat("Speed", rb.velocity.magnitude);
 	}
 
 	public void LoadData(GameData data)
