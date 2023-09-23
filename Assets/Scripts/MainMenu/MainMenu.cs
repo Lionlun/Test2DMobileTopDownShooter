f0da8f0765ec8210ef720aa38a582fb376c0a47b
Leo Lunev <loganlev@bk.ru>
2023-09-23 12:09:07 +0300

    Feat: Добавить сцену MainMenu и скрипт MainMenu.

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private Button continueButton;
+	private async void Start()
+	{
+		await Task.Delay(200);
+		if (!DataManager.Instance.HasGameData())
+		{
+			continueButton.interactable = false;
+		}
+	}
+	public void OnNewGamePressed()
+	{
+		DataManager.Instance.NewGame();
+		SceneManager.LoadSceneAsync(0);
+	}
+	public void OnContinuePressed()
+	{
+		SceneManager.LoadSceneAsync(0);
+	}
+
+	
+}
