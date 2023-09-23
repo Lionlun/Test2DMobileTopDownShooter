34c9ae89c440458d7ff249960b9354a99f4d938d
Leo Lunev <loganlev@bk.ru>
2023-09-23 12:19:44 +0300

    Fix: Удалить неиспользуемые файлы.

--- Assets/Scripts/Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Item/CreateNewItem")]
-public class Item : ScriptableObject
-{
-    public int Id;
-    public string Name;
-    public Sprite Icon;
-}
