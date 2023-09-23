f6f41840b5badd4e689e5e7acb40a9fb1bd0fd09
Leo Lunev <loganlev@bk.ru>
2023-09-23 11:52:15 +0300

    Feat: Добавить Inventory метод UpdateAllItems и реализовать в нем интерфейс IDataSave.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory: MonoBehaviour
+public class Inventory: MonoBehaviour, IDataSave
 {
 	public static List<Item> Items { get; private set; } = new List<Item>();
 	[SerializeField] private SlotDistribution slotsDistribution;
@@ -19,5 +19,26 @@ public class Inventory: MonoBehaviour
 	public void ListItems(Item item)
 	{
 		slotsDistribution.Add(item);
+	}
+
+	public void UpdateAllItems()
+	{
+		slotsDistribution.ClearAllSlots();
+
+		foreach (Item item in Items) 
+		{
+			slotsDistribution.Add(item);
+		}
+	}
+
+	public void LoadData(GameData data)
+	{
+		Items = data.PlayerItems;
+		UpdateAllItems();
+	}
+
+	public void SaveData(ref GameData data)
+	{
+		data.PlayerItems = Items;
 	}
 }
