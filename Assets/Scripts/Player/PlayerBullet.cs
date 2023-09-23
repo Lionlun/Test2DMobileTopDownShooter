7fe78c6dbe68bbf1e32ee3131eaaa506380a69d0
Leo Lunev <loganlev@bk.ru>
2023-09-23 12:09:54 +0300

    Feat: Добавить PlayerBullet.

---
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+	private int damage = 10;
+	private int speed = 5;
+
+	private void Update()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		transform.position += transform.right * speed * Time.deltaTime;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.GetComponent<EnemyBase>() != null)
+		{
+			Debug.Log("EnemyHit");
+			var enemyHealth = collision.gameObject.GetComponent<Health>();
+			enemyHealth.GetHit(damage);
+			Destroy(this.gameObject);
+		}
+	}
+}
